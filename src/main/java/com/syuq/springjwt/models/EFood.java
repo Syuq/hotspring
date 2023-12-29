cfd0823aeec69f2a67dfb2b67896853d2336fd1f
Syuq <tatd.wine@gmail.com>
2023-12-29 09:39:28 +0700

    Final zz

--- src/main/java/com/syuq/springjwt/models/EFood.java
+++
@@ -1,5 +0,0 @@
-package com.syuq.springjwt.models;
-
-public enum EFood {
-    Starter, HotBeverage, ColdBeverage, MainCourse, Breads, Desert
-}
--- src/main/java/com/syuq/springjwt/models/EOrder.java
+++
@@ -1,5 +0,0 @@
-package com.syuq.springjwt.models;
-
-public enum EOrder {
-        CREATED, DISPATCH, DELIVERED
-}
